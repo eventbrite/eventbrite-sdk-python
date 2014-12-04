742785716eeee07a47aba1b2d7e9ef3a6d719720
Daniel Greenfeld <danny@eventbrite.com>
2014-12-04 14:22:52 -0800

    Fixed problems with importing the version number

--- eventbrite/__init__.py
+++ eventbrite/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Daniel Greenfeld'
 __email__ = 'danny@eventbrite.com'
-from _version import __version__
+__version__ = '3.0.0-alpha1'
 
 from client import Eventbrite
 from utils import EVENTBRITE_API_URL
