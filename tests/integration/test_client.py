4fee839502eb053541a954f4f6b28f6ad6a40b93
Daniel Greenfeld <danny@eventbrite.com>
2014-12-04 15:56:47 -0800

    Got second client test functioning

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -13,13 +13,13 @@ from eventbrite.models import EventbriteObject
 from ..base import unittest
 
 try:
-    oauth_token = os.environ[u'OAUTH_TOKEN']
+    OAUTH_TOKEN = os.environ[u'OAUTH_TOKEN']
     skip_integration_tests = False
 except KeyError:
     skip_integration_tests = True
 
 try:
-    user_id = os.environ[u'USER_ID']
+    USER_ID = os.environ[u'USER_ID']
     skip_user_id_tests = False
 except KeyError:
     skip_user_id_tests = True
@@ -29,7 +29,7 @@ class TestClient(unittest.TestCase):
 
     @unittest.skipIf(condition=skip_integration_tests, reason='Needs an OAUTH_TOKEN')
     def test_api_get(self):
-        eventbrite = Eventbrite(oauth_token)
+        eventbrite = Eventbrite(OAUTH_TOKEN)
 
         payload = eventbrite.api("get", "/users/me/", {})
 
@@ -59,13 +59,13 @@ class TestClient(unittest.TestCase):
 class TestClientAccessMethods(unittest.TestCase):
 
     def setUp(self):
-        self.eventbrite = Eventbrite(oauth_token)
+        self.eventbrite = Eventbrite(OAUTH_TOKEN)
 
     @unittest.skipIf(condition=skip_integration_tests, reason='Needs an OAUTH_TOKEN')
     def test_get_user_me(self):
         evbobject = self.eventbrite.get_user()
 
-        # Do we get an evbobject?
+        # Did we get an EventbriteObject?
         self.assertTrue(isinstance(evbobject, EventbriteObject))
 
         # check attributes
@@ -75,9 +75,20 @@ class TestClientAccessMethods(unittest.TestCase):
         # check that an ID exists
         self.assertTrue(evbobject.get('id'))
 
+    @unittest.skipIf(condition=skip_user_id_tests, reason='Needs a USER_ID')
+    @unittest.skipIf(condition=skip_integration_tests, reason='Needs an OAUTH_TOKEN')
+    def test_get_user(self):
+        evbobject = self.eventbrite.get_user(USER_ID)
 
+        # Did we get an EventbriteObject?
+        self.assertTrue(isinstance(evbobject, EventbriteObject))
 
+        # check attributes
+        for attribute in ['id', 'pk', 'type', 'pagination', 'list', 'status_code']:
+            self.assertTrue(attribute in evbobject.__dict__.keys())
 
+        # check that the ID's match
+        self.assertEqual(evbobject.get('id'), USER_ID)
 
 
 if __name__ == '__main__':
