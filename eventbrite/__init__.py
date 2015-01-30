3fe208b0fc19e70dba41dceb92b493736fb3e04a
Daniel Greenfeld <danny@eventbrite.com>
2015-01-30 10:29:56 -0800

    Remove unnecessary import

--- eventbrite/__init__.py
+++ eventbrite/__init__.py
@@ -3,13 +3,9 @@ from __future__ import unicode_literals
 
 __author__ = 'Daniel Greenfeld'
 __email__ = 'danny@eventbrite.com'
-# __version__ = '3.0.0-alpha5'
 
 from ._version import __version__
 
-try:
-    from .client import Eventbrite
-    from .utils import EVENTBRITE_API_URL
-except ImportError:
-    # Avoiding the setup.py bootstrap problem
-    pass
+
+from .client import Eventbrite
+from .utils import EVENTBRITE_API_URL
